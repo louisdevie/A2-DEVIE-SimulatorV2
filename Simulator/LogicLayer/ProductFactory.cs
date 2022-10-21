5cdedf6074dbb1097c2b5c85da0fee193430c28f
Louis DEVIE A2 <louis.devie@iut-dijon.u-bourgogne.fr>
2022-10-21 15:02:15 +0200

    2.2 CREATION DU CODE DE LA FABRIQUE Implémentation de `ProductFactory`

---
+++ Simulator/LogicLayer/ProductFactory.cs
@@ -0,0 +1,33 @@
+﻿using LogicLayer.Products;
+using System.Reflection;
+
+namespace LogicLayer
+{
+    internal class ProductFactory
+    {
+        private Dictionary<string, ConstructorInfo> _constructors;
+
+        public ProductFactory()
+        {
+            this._constructors = new Dictionary<string, ConstructorInfo>();
+
+            this._constructors.Add("bike", typeof (Bike).GetConstructor(Array.Empty<Type>())!);
+            this._constructors.Add("car", typeof (Car).GetConstructor(Array.Empty<Type>())!);
+            this._constructors.Add("scooter", typeof (Scooter).GetConstructor(Array.Empty<Type>())!);
+        }
+
+        public IEnumerable<String> Types => this._constructors.Keys;
+
+        public Product CreateProduct(String type)
+        {
+            if (this._constructors.ContainsKey(type))
+            {
+                return (Product)this._constructors[type].Invoke(Array.Empty<object>());
+            }
+            else
+            {
+                throw new Exception($"No constructor for {type}");
+            }
+        }
+    }
+}
